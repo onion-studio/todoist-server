fccc088b1b2a3c334116448bd2aac76eca46bc3e
Kim Seungha <seungha.me@gmail.com>
2018-12-29 17:28:26 +0900

    CORSˆ

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('swagger', app, document)
-
+  app.enableCors()
   await app.listen(3000)
 }
 bootstrap()
