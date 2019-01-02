d188d40918580918d0ca0faadce1882cc21937a1
Kim Seungha <seungha.me@gmail.com>
2019-01-03 00:40:53 +0900

    ormconfig.js 변경
    - client charset 설정
    - entities 경로 변경

--- ormconfig-example/ormconfig.js
+++ ormconfig-example/ormconfig.js
@@ -5,18 +5,19 @@ let config = {
   username: 'todoist',
   password: 'todoist',
   database: 'todoist',
-  entities: ['src/**/**.entity{.ts,.js}'],
+  entities: ['src/entity/*.ts'],
   migrations: ['migration/**/*.ts'],
   cli: {
-    'entitiesDir': 'src/entity',
-    'migrationsDir': 'migration',
+    entitiesDir: 'src/entity',
+    migrationsDir: 'migration',
   },
+  charset: 'utf8mb4',
 }
 
 if (process.env.NODE_ENV === 'test') {
   config = {
     ...config,
-    ...require('./ormconfig.test')
+    ...require('./ormconfig.test'),
   }
 }
 
